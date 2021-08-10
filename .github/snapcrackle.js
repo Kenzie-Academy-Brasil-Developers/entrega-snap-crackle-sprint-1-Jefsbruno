5f0d3ef0311023277d2c46f30e35634ab36fd18f
jeferson bruno <jeferson.bruno@live.com>
2021-08-09 22:15:38 -0300

    inserido numeros impares com snap

---
+++ .github/snapcrackle.js
@@ -0,0 +1,22 @@
+function snapCrackle(maxValue){
+
+let teste =[] 
+let numind=0
+
+for(let i = 1;i<=maxValue;i++){
+teste.push(i)
+    }while(numind<teste.length){
+        if((teste[numind]%2)!=0){
+            teste[numind]="Snap"
+        }numind+=1}
+return teste
+}
+
+
+
+
+
+
+
+
+
