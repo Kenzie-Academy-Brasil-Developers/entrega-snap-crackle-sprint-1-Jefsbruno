55679ca97c5dd174903b79d2fafd7453b45eda85
jeferson bruno <jeferson.bruno@live.com>
2021-08-09 23:11:36 -0300

    SnapCrackle e snap corretos, faltas as demais

--- .github/snapcrackle.js
+++ .github/snapcrackle.js
@@ -2,21 +2,16 @@ function snapCrackle(maxValue){
 
 let teste =[] 
 let numind=0
+let impar=maxValue%2
+let mult=maxValue%5
 
 for(let i = 1;i<=maxValue;i++){
 teste.push(i)
     }while(numind<teste.length){
-        if((teste[numind]%2)!=0){
-            teste[numind]="Snap"
-        }numind+=1}
-return teste
+        if((teste[numind]%2!=0)&&(teste[numind]%5==0)){
+        teste[numind]="SnapCrackle"
+    }else if ((teste[numind]%2!=0)){
+        teste[numind]="Snap"
+    }numind+=1
+}return teste
 }
-
-
-
-
-
-
-
-
-
